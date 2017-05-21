4570fa844b6902b0996660575a7c2618df0b58d6
Yilin Yang <yiliny@umich.edu>
2017-05-21 17:13:23 -0400

    Add checks to install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ `dirname $0` == '.' ]; then
+	echo "Don't run from this directory (doesn't work and Yilin is lazy!)"
+	exit
+fi
 
 # Install Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -14,6 +18,7 @@ mv ~/.vimrc ~/.vimrc.bak
 # Create symlink to this .vimrc
 ln -s `dirname $0`/.vimrc ~/.vimrc
 
+mkdir ~/.config
 mkdir ~/.config/nvim
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
