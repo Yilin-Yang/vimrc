60da94f44d73fde78f1e43afbfc5c342ef231758
Yilin Yang <yiliny@umich.edu>
2017-05-20 23:36:23 -0400

    Install script now sets up symlinks

--- install.sh
+++ install.sh
@@ -8,6 +8,12 @@ sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install neovim
 
+# Delete backup vimrc if you need to
+mv ~/.vimrc ~/.vimrc.bak
+
+# Create symlink to this .vimrc
+ln -s `dirname $0` ~/.vimrc
+
 mkdir ~/.config/nvim
 ln -s ~/.vim ~/.config/nvim
 ln -s ~/.vimrc ~/.config/nvim/init.vim
