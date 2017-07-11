8f4bd2b60889300dccac5308973e2819d2359ccb
Yilin Yang <yiliny@umich.edu>
2017-07-11 03:58:52 -0400

    Install yamllint, add more includes

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 if [ `dirname $0` == '.' ]; then
 	echo "Don't run from this directory (doesn't work and Yilin is lazy!)"
+	echo "Clone vimrc into a folder in your home folder, then run from your home folder!"
 	exit
 fi
 
@@ -11,6 +12,10 @@ git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 sudo add-apt-repository ppa:neovim-ppa/stable
 sudo apt update
 sudo apt install neovim
+
+# Install yamllint
+#	Should work automatically with syntastic and neomake
+sudo apt install yamllint
 
 # Delete backup vimrc if you need to
 mv ~/.vimrc ~/.vimrc.bak
