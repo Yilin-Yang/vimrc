930ecdc8d4bc7b017da3802f3c5a7c3c727031f9
Yilin Yang <yiliny@umich.edu>
2018-01-09 12:18:36 -0500

    Search dynamically for clang resources

--- cquery_setup.sh
+++ cquery_setup.sh
@@ -11,11 +11,20 @@ if [ ! -f $SETTINGS_FILE ]; then
     touch $SETTINGS_FILE
 fi
 
+# Ideally, cquery should already be installed. Try to find the clang version it uses.
+CQUERY_PREFIX_DIR="~/.local/stow/cquery"
+CLANG_DIR="`ls $CQUERY_PREFIX_DIR/lib`"
+if [ $CLANG_DIR ]; then
+    RESOURCE_DIR="$CQUERY_PREFIX_DIR/lib/$CLANG_DIR"
+else
+    RESOURCE_DIR="$CQUERY_PREFIX_DIR/lib/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04"
+fi
+
 cat << EOF >> $SETTINGS_FILE
 {
     "initializationOptions": {
         "cacheDirectory": "/tmp/cquery"
-        "resourceDirectory": "~/.local/stow/cquery/lib/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04"
+        "resourceDirectory": $RESOURCE_DIR
     }
 }
 EOF
