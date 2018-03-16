7bb618178041e8e9f4df8f6a03e9cac436f1fdcb
Yilin Yang <yiliny@umich.edu>
2018-03-16 04:18:44 -0400

    Make fix-lldb handle lldb-6.0

--- fix-lldb.sh
+++ fix-lldb.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Taken from: https://stackoverflow.com/a/31005690
-# Modified to use libLLVM-3.8 rather than 3.6, since these files are in 3.8
+# Modified to use libLLVM-6.0 rather than 3.6, since these files are in 6.0
 #   on my personal machine.
 
-sudo apt-get install -y lldb
+#sudo apt-get install -y lldb
 
-cd /usr/lib/llvm-3.8/lib/python2.7/site-packages/lldb
+cd /usr/lib/llvm-6.0/lib/python2.7/site-packages/lldb
 sudo rm _lldb.so
 sudo ln -s ../../../liblldb.so.1 _lldb.so
-sudo rm libLLVM-3.8.0.so.1
-sudo ln -s ../../../libLLVM-3.8.0.so.1 libLLVM-3.8.0.so.1
-sudo rm libLLVM-3.8.so.1
-sudo ln -s ../../../libLLVM-3.8.0.so.1 libLLVM-3.8.so.1
-export PYTHONPATH='/usr/lib/llvm-3.8/lib/python2.7/site-packages'
+sudo rm libLLVM-6.0.0.so.1
+sudo ln -s ../../../libLLVM-6.0.0.so.1 libLLVM-6.0.0.so.1
+sudo rm libLLVM-6.0.so.1
+sudo ln -s ../../../libLLVM-6.0.0.so.1 libLLVM-6.0.so.1
+export PYTHONPATH='/usr/lib/llvm-6.0/lib/python2.7/site-packages'
