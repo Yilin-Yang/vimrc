99fa5173177b59ee410ee4b08a9c2de5e500bee8
Yilin Yang <yiliny@umich.edu>
2019-04-02 16:41:36 -0400

    Clean up cquery setup script

--- cquery_setup.sh
+++ cquery_setup.sh
@@ -12,19 +12,10 @@ if [ ! -f $SETTINGS_FILE ]; then
 fi
 
 # Ideally, cquery should already be installed. Try to find the clang version it uses.
-CQUERY_PREFIX_DIR="$HOME/.local/stow/cquery"
-CLANG_DIR="`ls $CQUERY_PREFIX_DIR/lib`"
-if [ "$CLANG_DIR" ]; then
-    RESOURCE_DIR="$CQUERY_PREFIX_DIR/lib/$CLANG_DIR"
-else
-    RESOURCE_DIR="$CQUERY_PREFIX_DIR/lib/clang+llvm-4.0.0-x86_64-linux-gnu-ubuntu-14.04"
-fi
-
 cat << EOF >> $SETTINGS_FILE
 {
     "initializationOptions": {
         "cacheDirectory": "/tmp/cquery",
-        "resourceDirectory": "$RESOURCE_DIR"
     }
 }
 EOF
