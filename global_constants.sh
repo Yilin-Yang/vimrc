0baf85f99db3379b058b5a7208272ff70dce0c48
Yilin Yang <yiliny@umich.edu>
2019-06-01 00:12:38 -0400

    Use termux apt command for install
    There is no sudo on termux, but there is apt!

--- global_constants.sh
+++ global_constants.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 YILINYGIT="https://yiliny@bitbucket.org/yiliny"
-INSTALLCMD="sudo apt-get install -y"
+INSTALLCMD="apt install -y"
 
 # Start from Home Folder
 cd ~/
