8c1a4823fc1ce47e0802712fb2e61a8a127b99a4
Yilin Yang <yiliny@umich.edu>
2023-08-31 12:41:09 -0400

    Fix issue with install.sh script
    - Backup ~/.vim if one already exists
    - `ln -s [LOCATION] [TARGET]`, so ~/.vimrc should be the last positional
      argument

--- neovim.sh
+++ neovim.sh
@@ -7,9 +7,13 @@ $INSTALLCMD neovim ripgrep
 # Symlink nvim and vim's user-accessible config folders to the top-level of
 # this repo
 mkdir -p ~/.config
-mkdir -p ~/.vim
 ln -s ~/vimrc ~/.config/nvim
+
+if [ -d "~/.vim" ]; then
+  mv ~/.vim ~/.vim.bak
+fi
+# We want ~/.vim to "be" our ~/vimrc repo.
 ln -s ~/.config/nvim ~/.vim
 
 # Can't use init.vim alongside init.lua because neovim will complain
-ln -s ~/.vimrc ~/.config/nvim/vimrc
+ln -s ~/.config/nvim/vimrc ~/.vimrc
